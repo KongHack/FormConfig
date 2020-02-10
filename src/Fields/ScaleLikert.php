6f4f95b987644689905974009a42fc43cc7a0fc2
GameCharmer <1208565+GameCharmer@users.noreply.github.com>
2020-02-10 12:19:44 -0500

    Disable standard label on likert pending new pull request

--- src/Fields/ScaleLikert.php
+++ src/Fields/ScaleLikert.php
@@ -32,7 +32,7 @@ class ScaleLikert extends Base implements FieldInterface
      */
     public static function isStandardLabel(): bool
     {
-        return true;
+        return false;
     }
 
     /**
