21d52061b77df937c6e214490482e2b88a495df8
GameCharmer <1208565+GameCharmer@users.noreply.github.com>
2026-02-27 00:57:23 -0500

    Radio Table

---
+++ src/Fields/RadioTable.php
@@ -0,0 +1,74 @@
+<?php
+namespace GCWorld\FormConfig\Fields;
+
+use GCWorld\FormConfig\Abstracts\Base;
+use GCWorld\FormConfig\Core\Twig;
+use GCWorld\FormConfig\FieldInterface;
+use GCWorld\FormConfig\Forms\FormField;
+use GCWorld\FormConfig\Traits\Options;
+
+/**
+ * Class RadioTable
+ */
+class RadioTable extends Base implements FieldInterface
+{
+    use Options;
+
+    /**
+     * @return string
+     */
+    public static function getConstantName(): string
+    {
+        return 'RADIO_TABLE';
+    }
+
+    /**
+     * @return string
+     */
+    public static function getKey(): string
+    {
+        return 'radioTable';
+    }
+
+    /**
+     * @return bool
+     */
+    public static function isStandardLabel(): bool
+    {
+        return false;
+    }
+
+    /**
+     * @return bool
+     */
+    public static function isStandardGrouping(): bool
+    {
+        return true;
+    }
+
+    /**
+     * @return string
+     */
+    public static function getTwigPath(): string
+    {
+        return '@'.Twig::TWIG_NAMESPACE_REPLACE.'/fields/radioTable.twig';
+    }
+
+    /**
+     * @param FormField $field
+     * @return FormField
+     */
+    public static function makeReadOnly(FormField $field): FormField
+    {
+        if (array_key_exists($field->getValue(), $field->getOptions())) {
+            $opts = $field->getOptions();
+            $field->setType(StaticInput::getKey())
+                ->setValue($opts[$field->getValue()])
+                ->setOptions([]);
+        } else {
+            $field->setType(StaticInput::getKey())->setOptions([])->setValue('- Not Set -');
+        }
+
+        return $field;
+    }
+}
