c99d587407c840ca9fed208d44fcd662f02f0ebb
GameCharmer <1208565+GameCharmer@users.noreply.github.com>
2026-08-05 08:55:05 -0400

    Select Ajax Single Once

---
+++ src/Fields/SelectAjaxSingleOnce.php
@@ -0,0 +1,81 @@
+<?php
+namespace GCWorld\FormConfig\Fields;
+
+use GCWorld\FormConfig\Abstracts\Base;
+use GCWorld\FormConfig\Core\Twig;
+use GCWorld\FormConfig\FieldInterface;
+use GCWorld\FormConfig\Forms\FormField;
+use GCWorld\FormConfig\Traits\Ajax;
+use GCWorld\FormConfig\Traits\AutoComplete;
+use GCWorld\FormConfig\Traits\Options;
+use GCWorld\FormConfig\Traits\Select2;
+
+/**
+ * Class SelectAjaxSingleOnce
+ */
+class SelectAjaxSingleOnce extends Base implements FieldInterface
+{
+    use Ajax;
+    use Options;
+    use Select2;
+    use AutoComplete;
+
+    /**
+     * @return string
+     */
+    public static function getConstantName(): string
+    {
+        return 'SELECT_AJAX_SINGLE_ONCE';
+    }
+
+    /**
+     * @return string
+     */
+    public static function getKey(): string
+    {
+        return 'selectAjaxSingleOnce';
+    }
+
+    /**
+     * @return bool
+     */
+    public static function isStandardLabel(): bool
+    {
+        return true;
+    }
+
+    /**
+     * @return bool
+     */
+    public static function isStandardGrouping(): bool
+    {
+        return true;
+    }
+
+    /**
+     * @return string
+     */
+    public static function getTwigPath(): string
+    {
+        return '@'.Twig::TWIG_NAMESPACE_REPLACE.'/fields/selectAjaxSingleOnce.twig';
+    }
+
+    /**
+     * @param FormField $field
+     *
+     * @return FormField
+     */
+    public static function makeReadOnly(FormField $field): FormField
+    {
+        if (array_key_exists($field->getValue(), $field->getOptions())) {
+            $opts = $field->getOptions();
+            $field->setType(StaticInput::getKey())
+                ->setValue($opts[$field->getValue()])
+                ->setOptions([]);
+        } else {
+            $field->setType(StaticInput::getKey())->setOptions([])->setValue('- Not Set -');
+        }
+
+        return $field;
+    }
+}
